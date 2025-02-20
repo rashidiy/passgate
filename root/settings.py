071c23865025e97d076146d8a183e1229bb49091
rashidiy <bubbless7456@gmail.com>
2025-02-20 14:24:35 +0500

    update footer

--- root/settings.py
+++ root/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'root.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': ['templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
