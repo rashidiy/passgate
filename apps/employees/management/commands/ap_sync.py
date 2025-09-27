8b8b4ae3cbd65a7d708a8c10086f852659804456
rashidiy <bubbless7456@gmail.com>
2025-09-27 07:20:33 +0500

    simplify naming of command ap_cross_synchronize.py

--- apps/employees/management/commands/ap_sync.py
+++ apps/employees/management/commands/ap_sync.py
@@ -18,7 +18,7 @@ class Command(BaseCommand):
                 try:
                     ap, created = await AccessPoint.objects.aget_or_create(employee=employee, device=device)
                     if not created:
-                        await model_actions_receiver.__access_point_post_save(ap, created)
+                        await model_actions_receiver.access_point_post_save(None, ap, created)
                 except Exception as e:
                     print(f'Error occurred while creating access point: {e}.')
 
