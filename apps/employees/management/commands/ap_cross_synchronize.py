2992bbc7c1a805482b6abfb3cdf2c5894cf7f754
rashidiy <bubbless7456@gmail.com>
2025-09-27 07:14:31 +0500

    update bug on ap_cross_synchronize

--- apps/employees/management/commands/ap_cross_synchronize.py
+++ apps/employees/management/commands/ap_cross_synchronize.py
@@ -4,17 +4,21 @@ from django.core.management import BaseCommand
 
 from devices.models import Device
 from employees.models import Employee, AccessPoint
+from utils import model_actions_receiver
 
 
 class Command(BaseCommand):
     help = "no help text"
 
     async def sync(self):
+        devices = (device async for device in Device.objects.all())
         async for employee in Employee.objects.all():
             await asyncio.sleep(0.2)
-            async for device in Device.objects.all():
+            for device in devices:
                 try:
-                    await AccessPoint.objects.acreate(employee=employee, device=device)
+                    ap, created = await AccessPoint.objects.aget_or_create(employee=employee, device=device)
+                    if not created:
+                        await model_actions_receiver.__access_point_post_save(ap, created)
                 except Exception as e:
                     print(f'Error occurred while creating access point: {e}.')
 
